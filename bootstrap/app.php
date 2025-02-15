5402b5f0e4ee17b97d26f633e6e244fee5c316ac
Ali Hassan <Ali.hassan@ncit.gov.mv>
2025-02-15 15:12:06 +0500

    chore: Configure trusted proxies for handling forwarded headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,23 +1,34 @@
-<?php
+    <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+    use Illuminate\Foundation\Application;
+    use Illuminate\Foundation\Configuration\Exceptions;
+    use Illuminate\Foundation\Configuration\Middleware;
+    use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-        ]);
+    return Application::configure(basePath: dirname(__DIR__))
+        ->withRouting(
+            web: __DIR__ . '/../routes/web.php',
+            commands: __DIR__ . '/../routes/console.php',
+            health: '/up',
+        )
+        ->withMiddleware(function (Middleware $middleware) {
+            $middleware->web(append: [
+                \App\Http\Middleware\HandleInertiaRequests::class,
+                \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+            ]);
+            $middleware->trustProxies(
+                headers: Request::HEADER_X_FORWARDED_FOR |
+                    Request::HEADER_X_FORWARDED_HOST |
+                    Request::HEADER_X_FORWARDED_PORT |
+                    Request::HEADER_X_FORWARDED_PROTO |
+                    Request::HEADER_X_FORWARDED_AWS_ELB
+            );
+            $middleware->trustProxies(at: [
+                '165.232.186.50',
+            ]);
 
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+            //
+        })
+        ->withExceptions(function (Exceptions $exceptions) {
+            //
+        })->create();
