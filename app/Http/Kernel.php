eadd9d976b7fe972c48abd2b0e4e57959e51c7a2
Ali Hassan <Ali.hassan@ncit.gov.mv>
2025-02-15 05:57:49 +0500

    feat: Add Docker configurations for PHP 8.0 and 8.1 with Sail support

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,6 +25,8 @@ class Kernel extends HttpKernel
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\HandleInertiaRequests::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+
             // Web middleware group
         ],
 
