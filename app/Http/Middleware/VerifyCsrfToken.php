9dcda7770873a7419c05261d268c295d2d9c0af8
Ali Hassan <Ali.hassan@ncit.gov.mv>
2025-02-15 06:50:11 +0500

    chore: Add CSRF token debugging and configure Axios CSRF handling

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Closure;
+use Illuminate\Http\Request;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -14,4 +16,16 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    public function handle($request, Closure $next)
+    {
+        // Add debugging
+        \Log::info('CSRF Token from request:', [
+            'token' => $request->header('X-CSRF-TOKEN'),
+            'xsrf' => $request->header('X-XSRF-TOKEN'),
+            'cookie' => $request->cookie('XSRF-TOKEN'),
+        ]);
+
+        return parent::handle($request, $next);
+    }
 }
