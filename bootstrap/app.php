d88d9f003cfa25b1792ebf43601883e07d575353
ali <alix35301@gmail.com>
2025-12-08 01:05:36 +0500

    fix: migration error fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,38 +1,26 @@
-    <?php
+<?php
 
-    use Illuminate\Foundation\Application;
-    use Illuminate\Foundation\Configuration\Exceptions;
-    use Illuminate\Foundation\Configuration\Middleware;
-    use Illuminate\Http\Request;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-    return Application::configure(basePath: dirname(__DIR__))
-        ->withRouting(
-            api: __DIR__ . '/../routes/api.php',
-            commands: __DIR__ . '/../routes/console.php',
-            health: '/up',
-        )
-        ->withMiddleware(function (Middleware $middleware) {
-            $middleware->web(append: [
-                \App\Http\Middleware\HandleInertiaRequests::class,
-                \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-            ]);
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+        ]);
 
-            $middleware->alias([
-                'admin' => \App\Http\Middleware\AdminMiddleware::class,
-                'student' => \App\Http\Middleware\StudentMiddleware::class,
-            ]);
-
-            // // Use numeric values for proxy headers
-            // $middleware->trustProxies(headers:
-            //     0x1 | // X_FORWARDED_FOR
-            //     0x2 | // X_FORWARDED_HOST
-            //     0x4 | // X_FORWARDED_PORT
-            //     0x8 | // X_FORWARDED_PROTO
-            //     0x10  // X_FORWARDED_AWS_ELB
-            // );
-
-            // $middleware->trustProxies(at: '*');
-        })
-        ->withExceptions(function (Exceptions $exceptions) {
-            //
-        })->create();
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'student' => \App\Http\Middleware\StudentMiddleware::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
